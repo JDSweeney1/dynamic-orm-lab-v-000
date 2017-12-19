df39501d8963f5e733ad6c1e0a69e839ed82dcc9
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-19 00:26:01 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,6 +54,7 @@ class InteractiveRecord
   end
 
   def self.find_by(hash)
+    DB[:conn].results_as_hash = true
     hash.each do |key, value|
       sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
       DB[:conn].execute(sql)
